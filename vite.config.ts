352c50842080f462a78b48f737411b6ba4d92e84
Eduardo BR <ebrauta@gmail.com>
2026-07-15 17:29:02 -0300

    fix: path alias removed (using relative import for now - future implementaion)

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@models': path.resolve(__dirname, './src/models'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@utils': path.resolve(__dirname, './src/utils')
-    }
-  }
+  plugins: [react(), tailwindcss()]
 })

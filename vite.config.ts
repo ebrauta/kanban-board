49fa1f9a77ffb5fffe422b2b4dcdd254a4f8d667
Eduardo BR <ebrauta@gmail.com>
2026-07-15 15:33:58 -0300

    chore: configure path aliases and install uuid

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@utils': path.resolve(__dirname, './src/utils')
+    }
+  }
 })
